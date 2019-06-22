7d68dc209d479803fa42aa410551a919e2a8b587
Auto <Auto@NSWSYDNLP327.planit.local>
2019-06-22 17:04:27 +1000

    added comments

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -6,6 +6,7 @@ public class test {
 
 	public static void main(String[] args) {
 		
+		//configure webdriver 
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Auto\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
@@ -19,6 +20,7 @@ public class test {
 		//get the actual title of the webpage
 		actualTitle = driver.getTitle();
 		
+		//check page title
 		if(actualTitle.contentEquals(expectedTitle)) {
 			System.out.println("Test Passed!");
 		}else {
